5f0af9f8e4e2a04d374f75a67da177e25f0c49ba
iddrisu-abdul-razak <110116005+iddrisu-abdul-razak@users.noreply.github.com>
2022-08-09 19:40:26 +0000

    second commit

---
+++ main.js
@@ -0,0 +1,24 @@
+//logical operators
+if(3==3 && 4==4){
+    document.write("the condition is true.")
+}
+
+if (3==3 && 4==6){
+    document.write("the statement is false and will not be printed")
+}
+ const age =21;
+ if(age>18){
+    alert("old enough to register");
+ }else{
+    alert("user is not old enough to enter");
+ }
+  
+ const x =16;
+ if(x< 10){
+document.getElementById("demo2").innerHTML="x is less than 10";
+ }else if(x> 10){
+    document.getElementById("demo2").innerHTML="x is greater than 10";
+ }else{
+    document.getElementById("demo2").innerHTML="x is 10,it is neither greater or less";
+ }
+ 
